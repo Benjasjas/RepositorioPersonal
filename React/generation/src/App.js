009b3f07657b5c792e9eb75918c7aada0cddc8ce
Benjamin Bustos Matus <benjaminbustosmatus@gmail.com>
2022-12-05 16:13:01 -0300

    "clases react"

---
+++ React/generation/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola mundo sureño</h1>
+      <p>Esto es un parrafo, que loco no</p>
+      <div>
+        <h2>este es otro div</h2>
+      </div>
+      <div>
+        <h3>y este es otro más</h3>
+      </div>
+    </div>
+  );
+};
+export default App;
